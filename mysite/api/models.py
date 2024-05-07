3cec2634132a5fd5eb0a96f52900a6303bcc9abb
nijiyamaharjan <nijiyamn@gmail.com>
2024-05-07 18:42:32 +0545

    deleted images

--- mysite/api/models.py
+++ mysite/api/models.py
@@ -1,13 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
 
-
-
-
-
-
-
-
 # Create your models here.
 class UploadImage(models.Model):
     picture = models.ImageField(upload_to="images/", blank = True)
